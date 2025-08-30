16763599124e7eae087c1db535fe71ae8f5b9de7
Rishabh Tiwari <rt8932484@gmail.com>
2025-08-30 18:45:51 +0000

    comments escape sequence & print in python

---
+++ p1.py
@@ -0,0 +1,13 @@
+#in first day we study about print in python
+print("hello ")
+print(22121*12)
+print("bye") 
+...
+hi
+...
+# in second day we study comments , escape sequence & print in python 
+
+print("hey i am a good boy \nand this viewer is also a good boy  ") 
+# in this i use to break line \n it break the line 
+
+
