7466eddcc64d95dc6b1f8e6d54b4bfb9ef04a380
Rishabh Tiwari <rt8932484@gmail.com>
2025-09-03 03:19:33 +0000

    variable datatype

---
+++ var_datatype.py
@@ -0,0 +1,23 @@
+a = 1.1
+b = "Rishabh"
+c = True
+d = None
+e = complex (8, 2)# it is complex data type 
+print(a) 
+print(b) 
+print(c) 
+print(d) 
+print(e)
+print("The type of b is :", type(b))# here type is use to know what datatype is here 
+print("The type of a is :", type(a))
+print("The type of c is :", type(c))
+print("The type of d is :", type(d))
+print("The type of e is :", type(e))
+
+list1 = [8, 2.3, [-4, 5.5], ["apple", "banana"]]
+print(list1)
+cuple = (("Parrot", "sparrow"), 
+("lion", "Tiger"))
+print(tuple1)
+dict1 = { "name": "Rishabh", "Age": 20, "canvote" : True}
+print(dict1)
